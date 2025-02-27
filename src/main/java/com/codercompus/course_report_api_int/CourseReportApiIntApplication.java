789cc504ef25f2ca600b31e07309f2fbedad3996
Mchedi <miled.chedi@esprit.tn>
2025-02-27 14:36:35 +0100

    test

---
+++ src/main/java/com/codercompus/course_report_api_int/CourseReportApiIntApplication.java
@@ -0,0 +1,13 @@
+package com.codercompus.course_report_api_int;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseReportApiIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseReportApiIntApplication.class, args);
+	}
+
+}
