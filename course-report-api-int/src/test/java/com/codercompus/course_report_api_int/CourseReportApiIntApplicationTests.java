46bc9757eb85ae2d3c55734ddcef446d922ac9a7
Mchedi <114103867+Mchedi@users.noreply.github.com>
2025-02-27 14:35:40 +0100

    Add files via upload

---
+++ course-report-api-int/src/test/java/com/codercompus/course_report_api_int/CourseReportApiIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codercompus.course_report_api_int;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseReportApiIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
